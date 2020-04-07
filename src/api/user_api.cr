df607fbd980706e4671864b10a34fbd3de89699b
Grabli66 <grabli66@gmail.com>
2020-04-07 14:49:20 +0300

    Login mail api

--- src/api/user_api.cr
+++ src/api/user_api.cr
@@ -1,1 +1,21 @@
 require "../database/database"
+require "../common/common_response_codes"
+
+# Проводит аутентификацию по электронной почте и паролю
+# Возвращает либо идентификатор сессии
+# Или код ошибки
+get "/user/mailLogin" do |env|
+    login = env.params.json["login"]?.as?(String)
+    password = env.params.json["password"]?.as?(String)
+  
+    if login.nil? || password.nil?
+        next getCodeResponse(BAD_REQUEST_ERROR)
+    end
+
+    user = Database.instance.userDao.getUserByEmail(login)
+    if user.password == password
+
+    else
+
+    end
+end
--- src/database/dao/user_dao.cr
+++ src/database/dao/user_dao.cr
@@ -1,6 +1,21 @@
 require "./base_dao"
 
 # Для доступа к пользователям
-class FileDao < BaseDao
-    
+class UserDao < BaseDao
+    # Инициализирует таблицу
+    def initTable
+        # Создаёт таблицу для объявлений пользователя
+        db.exec(
+            "CREATE TABLE IF NOT EXISTS users
+                (
+                    user_id INTEGER PRIMARY KEY AUTOINCREMENT,                    
+                    email VARCHAR(255)                    
+                )
+            ")
+    end
+
+    # Возвращает пользователя по электронной почте
+    def getUserByEmail(email : String)
+
+    end
 end
